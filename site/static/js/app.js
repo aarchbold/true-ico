239ca18759b2b4023367686086f742032383a667
Andrew Archbold <pulsegrenade@gmail.com>
2018-09-19 22:20:27 -0700

    #time 1h 0m - adding locale selector widget

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -178,3 +178,42 @@ $(function(){
         });
     });
 });
+$.fn.localizr = function() {
+    var $context = $(this),
+        $current = $('.language-selector__current', $context),
+        $currentLang = $('.language-selector__current span', $context),
+        $arrow = $('.language-selector__arrow', $context),
+        $layer = $('.language-selector__layer', $context),
+        $languages = $('.language-selector__item', $context);
+
+    function toggleLayer() {
+        if ($layer.hasClass('-active')) {
+            $arrow.removeClass('-active');
+            $layer.removeClass('-active');
+            $layer.hide();
+        } else {
+            $layer.show();
+            setTimeout(function() {
+                $arrow.addClass('-active');
+                $layer.addClass('-active');
+            },100)
+        }
+    }
+
+    $current.click(function() {
+        toggleLayer();
+    });
+
+    $languages.click(function(e) {
+        console.log($(e.target).data('language'));
+        $currentLang.html($(e.target).data('language'));
+        toggleLayer();
+    });
+
+
+}
+
+$(function(){
+    console.log('Localizer');
+    $('.language-selector').localizr();
+});
