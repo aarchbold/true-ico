f6676ca88370914dd84ace603df3635ab71e45fd
Andrew Archbold <pulsegrenade@gmail.com>
2018-09-24 19:59:29 -0700

    adding query param support for localization

--- site/static/js/app.js
+++ site/static/js/app.js
@@ -404,9 +404,22 @@ $.fn.localizr = function() {
     });
 }
 
+var getParameterByName = function (name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
 $(function(){
     console.log('Localizer');
-    if (localStorage.getItem('trueLanguage')) {
+    if (getParameterByName('lang')) {
+        localStorage.setItem('trueLanguage',getParameterByName('lang').toLocaleUpperCase());
+        handleLocalizaion(getParameterByName('lang').toUpperCase());
+    } else if (localStorage.getItem('trueLanguage')) {
         handleLocalizaion(localStorage.getItem('trueLanguage'));
     }
     $('.language-selector').localizr();
